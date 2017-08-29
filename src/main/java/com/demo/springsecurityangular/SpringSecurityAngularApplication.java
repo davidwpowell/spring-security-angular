34627a989efb6bf383506e936231b2da9258a1f0
davidwpowell <david.w.powell90@gmail.com>
2017-08-29 15:49:59 -0500

    Initial commit

---
+++ src/main/java/com/demo/springsecurityangular/SpringSecurityAngularApplication.java
@@ -0,0 +1,12 @@
+package com.demo.springsecurityangular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityAngularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityAngularApplication.class, args);
+	}
+}
